dee0463c5b710cbdd0f3f29aa8a74ec1a0826e9a
Rithvik Pallamreddy <1017rithvik@gmail.com>
2025-07-16 12:36:49 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Logo width={80} height={80} />
+      </div>
+    </>
+  );
+}
